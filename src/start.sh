0d54b6b1c79afc5e1a88993d2f745650777497e6
Martin Slabber <martin@tsolo.io>
2025-06-20 20:26:06 +0200

    feat: Restart after every assembly.

--- src/start.sh
+++ src/start.sh
@@ -3,5 +3,11 @@
 # SPDX-FileCopyrightText: 2025 Tsolo.io
 #
 # SPDX-License-Identifier: Apache-2.0
-
-python3 /app/src/cycax_blender_worker/main.py
+set -eu
+while true
+do
+    # Run in an endless loop.
+    # The blender drawings leave artifacts behind,
+    # A simple solution is to restart the service after every drawing.
+    python3 /app/src/cycax_blender_worker/main.py
+done
